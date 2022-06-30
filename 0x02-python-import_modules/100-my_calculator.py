62f15c7c1f6ad717d917472bb9068cb98faab1a0
marieloujo <detchenoujoan@gmail.com>
2022-07-01 00:52:13 +0100

    Advanced task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    if sys.argv[2] not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    calculator = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    res = (int(sys.argv[1]), int(sys.argv[3]))
+
+    print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
