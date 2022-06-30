04cc141a5a7d2a04251d16c869a011f18dec8eb0
marieloujo <detchenoujoan@gmail.com>
2022-06-30 23:45:14 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{} arguments {}".format(argc, ('.' if argc == 0 else ':')))
+    print("{} arguments{}".format(argc, ('.' if argc == 0 else ':')))
 
     for index in range(1, argc + 1):
         print("{}: {}".format(index, sys.argv[index]))
