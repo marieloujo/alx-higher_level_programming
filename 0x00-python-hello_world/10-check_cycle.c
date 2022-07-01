1ce71cad77f8c54229e41f4bd2c6bd9289a9bf49
marieloujo <detchenoujoan@gmail.com>
2022-07-01 02:31:42 +0100

    Print number

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - hecks if a singly linked list has a cycle in it
+ * @list: pointer to head of list
+ * Return: number of nodes
+ */
+int check_cycle(listint_t *list)
+{
+    list = list;
+    return (0);
+}
