9a9a18ca511f75db8bea44e718d504a38002c342
marieloujo <detchenoujoan@gmail.com>
2022-07-04 17:38:29 +0100

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for item in zip(tuple_a, tuple_b):
-        print(item)
+    add_of_tuple = []
+    for i in range(2):
+        a = tuple_a[i] if len(tuple_a) >= (i + 1) else 0
+        b = tuple_b[i] if len(tuple_b) >= (i + 1) else 0
+        add_of_tuple.append(a + b)
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-""" add_tuple(tuple_a, tuple_b) """
-add_tuple(tuple_a, (1, ))
-""" add_tuple(tuple_a, ()) """
+    return tuple(add_of_tuple)
