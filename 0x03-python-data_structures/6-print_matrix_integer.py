8ca40a8d369bc2f1642874c44ca1ebf564d819ca
marieloujo <detchenoujoan@gmail.com>
2022-07-04 17:05:11 +0100

    Fix pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index in range(len(row)):
-            print("{:d}".format(row[index]), end='' if index == len(row) - 1 else ' ')
+            print("{:d}".format(row[index]),
+                  end='' if index == len(row) - 1 else ' ')
         print()
