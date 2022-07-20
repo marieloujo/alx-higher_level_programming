f583f63258e75b424ece6b664d085bb762246717
marieloujo <detchenoujoan@gmail.com>
2022-07-21 00:00:59 +0100

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """An Square class
+
+    Attributes:
+        attr1 (__size: int): Private instance attribute, size of obj od square
+    """
+
+    def __init__(self, size=0):
+        """The constructor of class
+
+        Args:
+            param1 (size: int): initialize value to size.
+
+        Returns:
+            None
+        """
+
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
