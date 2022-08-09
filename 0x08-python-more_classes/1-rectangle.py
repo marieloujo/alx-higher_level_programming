82ee9a598b018a0ef7d70023ddd4ee2b14b613d5
marieloujo <detchenoujoan@gmail.com>
2022-08-09 23:19:34 +0100

    Update task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,35 +28,34 @@ class Rectangle:
     def width(self):
         """Retrieve of private attribute width"""
         return self.__width
-    
+
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """Set the value of __width
 
         Args:
-            width (int): new width of square.
+            value (int): new width of square.
         """
-        if (type(width) is not int):
-            raise TypeError("size must be an integer")
-        if width < 0:
-            raise ValueError("size must be >= 0")
-        self.__width = width
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-    
     @property
     def height(self):
         """Retrieve of private attribute height"""
         return self.__height
-    
+
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """Set the value of __height
 
         Args:
-            height (int): new height of square.
+            value (int): new height of square.
         """
-        if (type(height) is not int):
-            raise TypeError("size must be an integer")
-        if height < 0:
-            raise ValueError("size must be >= 0")
-        self.__height = height
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
