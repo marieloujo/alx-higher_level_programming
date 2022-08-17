881fe9ade445e5514642403bf470a8a38fda5292
marieloujo <detchenoujoan@gmail.com>
2022-08-17 09:59:52 +0100

    Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,3 +39,7 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Square """
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
