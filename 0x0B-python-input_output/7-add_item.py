e1ff57e1eeec9e115ef47dbd388b09f70cb79484
marieloujo <detchenoujoan@gmail.com>
2022-08-17 01:04:59 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,18 +5,17 @@
 """
 
 
-import json, sys
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-    filename = 'add_item.json'
+filename = 'add_item.json'
+
+try:
+    my_list = load_from_json_file(filename)
+except:
     my_list = []
-    with open(filename, 'w', encoding="utf-8") as f:
-        try:
-            my_list = load_from_json_file(filename)
-        except json.JSONDecodeError:
-            pass
-        my_list.extend(sys.argv[1:])
-        save_to_json_file(my_list, filename)
+    pass
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
