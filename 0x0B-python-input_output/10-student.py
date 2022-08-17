68823cfd1497c1ec24db74bd676366c23a900050
marieloujo <detchenoujoan@gmail.com>
2022-08-17 01:26:24 +0100

    Task 9 & 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module 9-student.py """
+
+
+class Student:
+    """ Definition of class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Create new instance """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for attr in attrs:
+            try:
+                new_dict[attr] = self.__dict__[attr]
+            except BaseException:
+                pass
+        return new_dict
