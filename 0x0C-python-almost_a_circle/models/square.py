601d95394b7c5448085c7fb733f0ed72b30e36c8
marieloujo <detchenoujoan@gmail.com>
2022-08-17 09:46:29 +0100

    Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Create new square """
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve of private attribute __size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the value of __size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """ String representation of square """
