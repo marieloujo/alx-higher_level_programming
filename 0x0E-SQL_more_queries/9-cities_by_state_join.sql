89f2d07ca1b9dffb2ee55dd5f060a1ab3d2893ae
marieloujo <detchenoujoan@gmail.com>
2022-09-09 03:55:34 +0100

    Adding files

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 --  Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id;
-
+SELECT cities.id, cities.name, states.name FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id;
