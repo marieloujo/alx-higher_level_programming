e596b68fb10a76316d48a5b65945f0a7c345e792
marieloujo <detchenoujoan@gmail.com>
2022-09-09 01:49:36 +0100

    Adding comment

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -5,6 +5,7 @@
 ---     The database name will be passed as an argument of the mysql command
 ---     If the table first_table already exists, your script should not fail
 ---     You are not allowed to use the SELECT or SHOW statements
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT (10),
     name VARCHAR (50)
