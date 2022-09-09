760d6652d047e09850229b40b74769d63cba6486
marieloujo <detchenoujoan@gmail.com>
2022-09-09 04:21:05 +0100

    Adding files

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-    INNER JOIN tv_show_genres
+    LEFT JOIN tv_show_genres
         on tv_shows.id = tv_show_genres.show_id
     ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
